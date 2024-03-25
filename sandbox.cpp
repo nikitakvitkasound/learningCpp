d8a490043233cad58b352fdb99b9ec3a197a05f3
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2024-03-25 22:45:25 +0400

    Learning Cpp 6: Kinda trying modular programming

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 
+void Test2(){
+    std::cout << "Koook";
+}
+
+int Test1(int a, int b){
+    Test2();
+    return a + b;
+}
+
 int main(){
     std::cout << "hello!";
+    Test1(2, 5);
 
     return 0;
 }
