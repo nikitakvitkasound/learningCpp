899edf9f34ecdef3b108dc2965000e8930cae5f7
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2024-03-29 18:36:16 +0400

    Leaning Cpp 9: LeetCode problems

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> twoSum(std::vector<int>& nums, int target) {
-    std::vector<int> answer;
-    for (std::vector<int>::size_type i = 0; i < nums.size(); ++i) {
-        for (std::vector<int>::size_type j = i + 1; j < nums.size(); ++j) {
-            if (nums[i] + nums[j] == target) {
-                answer = {static_cast<int>(i), static_cast<int>(j)};
-                return answer;
-            }
-        }
+std::vector<int> runningSum(std::vector<int>& nums){
+    for(std::vector<int>::size_type i = 1; i < nums.size(); i++){
+        nums[i] += nums[i-1];
     }
-    return answer;
+    return nums;
 }
 
-int main() {
-    std::vector<int> input = {2, 7, 11, 15};
-    int sum = 9;
-    std::vector<int> pos = twoSum(input, sum);
-    std::cout << pos[0] << ", " << pos[1];
+void printVector(std::vector<int>& v){
+    std::cout << "[";
+    for (std::vector<int>::size_type i = 0; i < v.size(); ++i){
+        std::cout << " " << v[i];
+    }
+    std::cout << " ]";
+}
+
+int main(){
+    std::vector<int> nums = {1, 2, 3, 4};
+    std::vector<int> sum = runningSum(nums);
+    printVector(sum);
+
     return 0;
 }
