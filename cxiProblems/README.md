d585fc0d30bee1b91162a7c03555d76a76fcc724
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2024-04-03 21:39:21 +0400

    LearningCpp 14: Start CXI (111) problems list

---
+++ cxiProblems/README.md
@@ -0,0 +1,4 @@
+just solving problems from this list:
+https://github.com/Rustam-Z/cpp-programming/blob/main/README.md
+
+pretentiously named as cxiProblems (CXI is roman numbers for 111). Hint 111 Problems.
---
+++ cxiProblems/problem001.cpp
@@ -0,0 +1,13 @@
+// CXI Problems :: 1 :: C++ Program to print "Hello, World!".
+// Based on https://github.com/Rustam-Z/cpp-programming/blob/main/README.md list
+// by Nikita Kvitka as part of learningCpp (https://github.com/nikitakvitkasound/LearningCpp)
+
+#include <iostream> 
+
+int main(){
+    std::cout << "Hello, World!" << '\n';
+    
+    std::cout << "Press enter to exit...";
+    std::cin.get();
+    return 0;
+}
