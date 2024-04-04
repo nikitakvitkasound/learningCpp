3cc9864eb9504793fbeed383858dfecb48df0fa9
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2024-04-04 21:55:37 +0400

    LearningCpp 17: Solve problem 3

--- cxiProblems/problem003.cpp
+++ cxiProblems/problem003.cpp
@@ -5,61 +5,44 @@
 #include <iostream>
 #include <string>
 
-static void AskForInputAndValidate(const std::string& validate_to){
-    std::string input;
-    std::cout << "Type letter 'A', 'S', 'M' or 'D' and I will do the rest ('Q' to quit program): ";
-    std::cin >> input;
-}
-
 int main(){
     const int kA = 4;
     const int kB = 2;
-    const std::string commands = "ASMDQ";
-    //bool keep_calculate = true;
-    std::string user_command;
+    bool keep_calculate = true;
+    char user_command;
 
     std::cout << "Hello! Do you want to see my arithmetic skills?\n";
     std::cout << "I can easily Add/Subtract/Multiply/Divide " << kA << " and " << kB << '\n';
-    std::cout << "What do you want see first?\n";
+    std::cout << "Type 'A', 'S', 'M' or 'D' to do arithmetic operation ('Q' to quit the program) ";
+    
+    while (keep_calculate){
+        std::cin >> user_command;
+        user_command = static_cast<char>(toupper(user_command));
 
-    std::cin >> user_command;
-    std::cout << user_command[0] << '\n';
-        
-    //user_command = AskForInput();
-    // while (keep_calculate){
-    //     switch (user_command){
-    //     case 0: //A
-    //         std::cout << "If you add " << kA << " and " << kB << " you will get " << kA + kB << " as result\n";
-    //         std::cout << "\nDo you want to try other arithmetic operations?\n";
-    //         //user_command = AskForInput();
-    //         break;
-    //     case 1: //B
-    //         std::cout << "If you subtract " << kA << " from " << kB << " you will get " << kA - kB << " as result\n";
-    //         std::cout << "\nDo you want to try other arithmetic operations?\n";
-    //         //user_command = AskForInput();
-    //         break;
-    //     case 2: //M
-    //         std::cout << "If you multiply " << kA << " and " << kB << " you will get " << kA * kB << " as result\n";
-    //         std::cout << "\nDo you want to try other arithmetic operations?\n";
-    //         //user_command = AskForInput();
-    //         break;
-    //     case 3: //D
-    //         std::cout << "If you divide " << kA << " by " << kB << " you will get " << kA / kB << " as result\n";
-    //         std::cout << "\nDo you want to try other arithmetic operations?\n";
-    //         //user_command = AskForInput();
-    //         break;
-    //     case 4: //Q
-    //         keep_calculate = false;
-    //         break;
-    //     default:
-    //         std::cin.clear();
-    //         std::cin.ignore(100000, '\n');
-
-    //         std::cout << "[ERROR]: ";
-    //         user_command = AskForInput();
-    //     }
-    // }
-    std::cout << "Okay, that's all for today. Bye!\n";
+        switch (user_command){
+        case 'A': //A
+            std::cout << "If you add " << kA << " and " << kB << " you will get " << kA + kB << " as result\n";
+            break;
+        case 'S': //S
+            std::cout << "If you subtract " << kA << " from " << kB << " you will get " << kA - kB << " as result\n";
+            break;
+        case 'M': //M
+            std::cout << "If you multiply " << kA << " and " << kB << " you will get " << kA * kB << " as result\n";
+            break;
+        case 'D': //D
+            std::cout << "If you divide " << kA << " by " << kB << " you will get " << kA / kB << " as result\n";
+            break;
+        case 'Q': //Q
+            std::cout << "Okay, that's all for today. Bye!\n";
+            keep_calculate = false;
+            break;
+        default:
+            std::cout << "Type 'A', 'S', 'M' or 'D' to do arithmetic operation ('Q' to quit the program)\n";
+        }
+        if (keep_calculate){
+            std::cout << "Let's do one more . . .  ";
+        }
+    }
 
     system("pause");
     return 0;
