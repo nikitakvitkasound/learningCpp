cbc5de56e035793a2f1c4f540ebdf9d060c9b1e1
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2024-04-09 21:14:07 +0400

    learningCpp 22: Learn STL vector container and iterator

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,32 +1,16 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 int main(){
-    enum fields {WORD, HINT, NUM_FIELDS};
-    const int NUM_WORDS = 5;
-    std::string WORDS[NUM_WORDS][NUM_FIELDS] = {
-        {"joystick", "you use your hand to play with it"},
-        {"headphones", "you listen music with it"},
-        {"word3", "hint3"},
-        {"word4", "hint4"},
-        {"word5", "hint5"}
-    };
-
-    srand(static_cast<unsigned int>(time(0)));
-    int choice = (rand() % NUM_WORDS);
-    std::string hint = WORDS[choice][HINT];
-    std::string jumble = WORDS[choice][WORD];
-
-    std::cout << jumble;
-
-    for (unsigned int i = 0; i < jumble.size(); ++i){
-        size_t index_01 = (static_cast<size_t>(rand()) % jumble.size());
-        size_t index_02 = (static_cast<size_t>(rand()) % jumble.size());
-        char temp = jumble[index_01];
-        jumble[index_01] = jumble[index_02];
-        jumble[index_02] = temp;
+    std::vector<std::string> inventory(3);
+    inventory[0] = "sword";
+    inventory[1] = "arrows";
+    inventory[2] = "bow";
+    for(auto iter = inventory.begin(); iter < inventory.end(); ++iter){
+        std::cout << *iter << '\n';
     }
 
-    std::cout << jumble;
-
+    system("pause");
     return 0;
 }
