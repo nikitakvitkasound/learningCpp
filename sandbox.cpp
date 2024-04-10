02063952367d260340384f1c3ec925ad8f57f5a4
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2024-04-10 16:19:35 +0400

    learningCpp 23: Learn algorithms for vectors and other containers

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,16 +1,38 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 int main(){
-    std::vector<std::string> inventory(3);
-    inventory[0] = "sword";
-    inventory[1] = "arrows";
-    inventory[2] = "bow";
-    for(auto iter = inventory.begin(); iter < inventory.end(); ++iter){
+    // std::vector<std::string> inventory(3);
+    // inventory[0] = "sword";
+    // inventory[1] = "arrows";
+    // inventory[2] = "bow";
+    // for(auto iter = inventory.begin(); iter < inventory.end(); ++iter){
+    //     std::cout << *iter << '\n';
+    // }
+    // std::vector<std::string>::iterator item = inventory.begin();
+    // std::string phrase = "shield and sword are best friends";
+    // if(!(item->empty())){
+    //     if(phrase.find(*item) != std::string::npos){
+    //         inventory.insert(++item, "crossbow");
+    //         inventory.erase(inventory.end() - 1);
+    //     }
+    // }
+    // std::cout << "\n- - - update inventory - - -\n\n";
+    // for(auto iter = inventory.begin(); iter < inventory.end(); ++iter){
+    //     std::cout << *iter << '\n';
+    // }
+    // system("pause");
+    // return 0;
+    std::vector<int> numbers = {1, 23, 435, 21, 12, 3, 8, 10, 15, 44, 2};
+    sort(numbers.begin(), numbers.begin() + 7);
+    for (std::vector<int>::const_iterator iter = numbers.begin(); iter != numbers.end(); ++iter){
         std::cout << *iter << '\n';
     }
-
-    system("pause");
+    sort(numbers.begin(), numbers.end());
+    for (std::vector<int>::const_iterator iter = numbers.begin(); iter != numbers.end(); ++iter){
+        std::cout << *iter << '\n';
+    }
     return 0;
 }
