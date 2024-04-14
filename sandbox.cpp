a00049a97e66655a8751b4901518f6443a49f94a
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2024-04-14 12:12:54 +0400

    learning Cpp 28: Function's overload, lining etc

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
 
+//bool IsTrue(bool q);
+const int MAX = 8;
+int Sum(int a = 1, int b = MAX);
+double Sum (double a, double b);
+
 
 int main() {
     int x = 2;
-    if (x == 2){
-        int x = 10;
-        std::cout << x << '\n';
-    }{
-        int x = 22;
-        std::cout << "hello " << x << '\n';
-    }
-    std::cout << x << '\n';
+    int y = 12;
+    
+    double xx = 3.4;
+    double yy = 13.2;
+
+    std::cout << Sum() << '\n';
+    std::cout << Sum(x, y) << '\n';
+    std::cout << Sum(xx, yy) << '\n';
 
 
     system("pause");
     return 0;
 }
 
+/* - - - - - - - Declaration - - - - - - - */
+
+int Sum(int a, int b){
+    return a + b;
+}
+
+double Sum (double a, double b){
+    return a + b;
+}
