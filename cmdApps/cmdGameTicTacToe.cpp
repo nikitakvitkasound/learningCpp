3e641c9bac6a717449335743d958737e638ad5b3
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2024-04-18 21:43:08 +0400

    learningCpp 31: Minore update TicTacToe

--- cmdApps/cmdGameTicTacToe.cpp
+++ cmdApps/cmdGameTicTacToe.cpp
@@ -18,29 +18,29 @@
 #include <vector>
 #include <algorithm>
 
-const char X = 'X';
-const char O = 'O';
-const char EMPTY = ' ';
-const char TIE = 'T';
-const char NO_ONE = 'N';
+// const char X = 'X';
+// const char O = 'O';
+// const char EMPTY = ' ';
+// const char TIE = 'T';
+// const char NO_ONE = 'N';
 
 static void Instructions();
-static void DisplayBoard(const std::vector<char>& board);
-static void AnnounceWinner(char winner, char computer, char human);
-static char Winner(const std::vector<char>& board);
-static char HumanPiece();
-static char OpponentPiece(char piece);
-static char AskYesNo(std::string question);
-static int HumanMove(std::vector<char>& board, char human);
-static int ComputerMove(std::vector<char>& board, char computer);
-static int AskNumber(std::string question, int low = 0, int high);
-static bool IsLegal(const std::vector<char>& board, int move);
+// static void DisplayBoard(const std::vector<char>& board);
+// static void AnnounceWinner(char winner, char computer, char human);
+// static char Winner(const std::vector<char>& board);
+// static char HumanPiece();
+// static char OpponentPiece(char piece);
+// static char AskYesNo(std::string question);
+// static int HumanMove(std::vector<char>& board, char human);
+// static int ComputerMove(std::vector<char>& board, char computer);
+// static int AskNumber(std::string question, int low = 0, int high);
+// static bool IsLegal(const std::vector<char>& board, int move);
 
 
 int main(){
-    int move;
-    const int NUM_SQUARES = 9;
-    std::vector<char> board(NUM_SQUARES, EMPTY);
+    //int move;
+    //const int NUM_SQUARES = 9;
+    //std::vector<char> board(NUM_SQUARES, EMPTY);
 
     Instructions();
     // char human = HumanPiece();
@@ -63,15 +63,15 @@ int main(){
 /* - - - - - - Declaration - - - - - */
 void Instructions(){
     std::cout << "Welcome to the ultimate man-machine showdown: Tic-Tac-Toe.\n";
-    std::cout << "--where human brain is pit against silicon processor\n\n";
-    std::cout << "<ake your move known by entering a number, 0 - 8. The number\n";
-    std::cout << "corresponds to the desired board position, as illustrated:\n\n";
+    std::cout << "Where human brain is compit against silicon processor\n\n";
+    std::cout << "Take your move by entering a number: 0 - 8.\n";
+    std::cout << "The number corresponds to the desired board position, as illustrated:\n\n";
     
-    std::cout << " 0 | 1 | 2\n";
-    std::cout << " ---------\n";
-    std::cout << " 3 | 4 | 5\n";
-    std::cout << " ---------\n";
-    std::cout << " 6 | 7 | 8\n\n";
+    std::cout << " \t\t0 | 1 | 2\n";
+    std::cout << " \t\t---------\n";
+    std::cout << " \t\t3 | 4 | 5\n";
+    std::cout << " \t\t---------\n";
+    std::cout << " \t\t6 | 7 | 8\n\n";
 
     std::cout << "Prepare yourself, human. The battle is about to begin.\n\n";
 }
