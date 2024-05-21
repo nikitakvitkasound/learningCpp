55e12054df173ac97c218e558948e977a81151cf
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2024-05-21 18:47:08 +0400

    learningCpp 68: Solve problem 38

---
+++ cxiProblems/problem038.cpp
@@ -0,0 +1,22 @@
+// CXI Problems :: 38 :: C++ Program to Print Sequence 1
+// Based on https://github.com/Rustam-Z/cpp-programming/blob/main/README.md list
+// by Nikita Kvitka as part of learningCpp (https://github.com/nikitakvitkasound/learningCpp)
+// *** Problems are kind of redundant. From now on I will do simple implementation (without input validation and checking for corner cases until 'new' problem occurs).
+
+#include <iostream>
+
+int main(){
+    const char BRICK = '*';
+    const int SIZE = 5;
+    for(int i = 0; i < SIZE; ++i){
+        for(int j = 0; j <= i; ++j){
+            std::cout << BRICK;
+        }
+        std::cout << '\n';
+    }
+
+
+    std::cout << "\n";
+    system("pause");
+    return 0;
+}
