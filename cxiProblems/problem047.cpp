57ff5e0c065baa9c087defaf41e666494e967b14
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2024-07-05 21:29:40 +0400

    learning Cpp 82: Solve problem 47

---
+++ cxiProblems/problem047.cpp
@@ -0,0 +1,33 @@
+// CXI Problems :: 47 :: Program to add the first seven terms of the following series using a for loop: 1/1! + 2/2! + 3/3! + ...
+// Based on https://github.com/Rustam-Z/cpp-programming/blob/main/README.md list
+// by Nikita Kvitka as part of learningCpp (https://github.com/nikitakvitkasound/learningCpp)
+// *** Problems are kind of redundant. From now on I will do simple implementation (without input validation and checking for corner cases until 'new' problem occurs).
+
+// get number (N) until 7
+// calc n!
+// go through n! = n * (n - 1) ....
+// do 1 / n! 
+// and store result in var
+// repeat
+
+#include <iostream>
+
+int main(){
+    float num = 7.0f;
+    float sum = 0.0f;
+    
+    std::cout << "I can do sum of the series of 1/1! + 1/2! + 1/3! ... 1/n! Just input integer number: ";
+    std::cin >> num;
+
+    for(float i = num; i != 0; --i){
+        float fac = 1.0f;
+        for(float j = i; j != 0; --j){
+            fac *= j;
+        }
+        // std::cout << fac << '\n';
+        sum += 1 / fac;
+    }
+    
+    std::cout << ">>> " << sum;
+    return 0;
+}
