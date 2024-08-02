c5e5664b280207cc28aebaa9d9eafb5aee46d84f
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2024-08-02 23:16:19 +0400

    learningCpp 101: learnCpp article example exercise

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,26 +1,16 @@
 #include <iostream>
-#include <string>
-#include <cmath>
 
-double daily_rate(double hourly_rate) {
-    int billable_hours {8};
-    return hourly_rate * billable_hours;
+// assume int is 4 bytes
+void doSomething(unsigned int x)
+{
+    // Run some code x times
+
+    std::cout << "x is " << x << '\n';
 }
 
-int monthly_rate(double hourly_rate, double discount) {
-    int billable_days {22};
-    double monthly_rate = daily_rate(hourly_rate) * billable_days;
-    discount = monthly_rate * discount * 0.01;
-    return static_cast<int>(std::ceil(monthly_rate - discount));
-}
+int main()
+{
+    doSomething(1);
 
-int main(){
-
-    double hr {77.0};
-    double disc {10.5};
-
-    std::cout << monthly_rate(hr, disc) << '\n';
-
-    system("pause");
     return 0;
 }
