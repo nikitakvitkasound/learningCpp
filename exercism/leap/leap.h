4291e39d2638d06949c810a5b015d3418414f6d9
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2024-08-10 21:25:04 +0400

    learningCpp 104: leap problem

---
+++ exercism/leap/leap.h
@@ -0,0 +1,6 @@
+#ifndef __LEAP_H__
+#define __LEAP_H__
+namespace leap {
+    bool is_leap_year(int year);
+}
+#endif
--- sandbox.cpp
+++ sandbox.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 
-bool needs_license(std::string kind){
-    return kind == "car" || kind == "truck";
-}
-
-std::string choose_vehicle(std::string option1, std::string option2) {
-    if(option1 > option2){
-        return option2 + " is clearly the better choice.";
+bool is_leap_year(int year){
+    if (year % 100 == 0){
+        if(year % 400 == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    else if(year % 4 == 0){
+        return true;
     }
     else{
-        return option1 + " is clearly the better choice.";
+        return false;
     }
 }
 
 int main(){
-    std::string vehicle {"bike"};
-    std::cout << needs_license(vehicle);
-    std::string opt_1 = "Audi";
-    std::string opt_2 = "Toyota";
-    std::cout << choose_vehicle(opt_1, opt_2);
+    int y {2100};
+    std::cout << is_leap_year(y);
     
     return 0;
 }
