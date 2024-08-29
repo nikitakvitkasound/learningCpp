5e7ea7769c49213e030fafe99e0f70b3acf96ff3
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2024-08-29 19:31:18 +0400

    learningCpp 106: reverse string problem

---
+++ exercism/reverse-string/reverse_string.cpp
@@ -0,0 +1,8 @@
+#include "reverse_string.h"
+
+namespace reverse_string {
+    std::string reverse_string(std::string str){
+        std::reverse(str.begin(), str.end());
+        return str;
+    }
+}  
--- sandbox.cpp
+++ sandbox.cpp
@@ -1,49 +1,15 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 
-// interest_rate returns the interest rate for the provided balance.
-double interest_rate(double balance) {
-    double negative_rate {3.213};
-    double low_rate {0.5};
-    double mid_rate {1.621};
-    double high_rate {2.475};
-    if(balance < 0){
-        return negative_rate;
-    }
-    else if(balance < 1000){
-        return low_rate;
-    }
-    else if(balance < 5000){
-        return mid_rate;
-    }
-    else{
-        return high_rate;
-    }
-}
-
-double yearly_interest(double balance) {
-    return balance * interest_rate(balance) * 0.01;
-}
-
-// annual_balance_update calculates the annual balance update, taking into
-// account the interest rate.
-double annual_balance_update(double balance) {
-    return balance + yearly_interest(balance);
-}
-
-// years_until_desired_balance calculates the minimum number of years required
-// to reach the desired balance.
-int years_until_desired_balance(double balance, double target_balance) {
-    int years = 0;
-    while(balance < target_balance){
-        balance = annual_balance_update(balance);
-        years += 1;
-        std::cout << years << '\n';
-    }
-    return years;
+void reverse_string(std::string str){
+    std::reverse(original.begin(), .end());
 }
 
 int main(){
-    std::cout << annual_balance_update(100.5);
-    std::cout << years_until_desired_balance(100.0, 125.80);
+    std::string original {"tobor"};
+    std::reverse(original.begin(), original.end());
+    std::cout << original;
+
     return 0;
 }
