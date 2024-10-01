a81e17fb4a7b49808ec14d43b8a3d66789e0f62e
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2024-10-01 23:55:31 +0400

    learningCpp 117: classes

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,39 +1,24 @@
 #include <iostream>
 #include <cmath>
 
-int score(double x, double y);
-
 int main(){
-    double x = 10;
-    double y = 11;
+    class Alien {
+        //todo accept x and y
+        public:
+        Alien(int new_x_coordinate, int new_y_coordinate){
+            int x_coordinate = new_x_coordinate;
+            int y_coordinate = new_y_coordinate;
+        }
+        int x_coordinate { };
+        int y_coordinate { };
+        
 
-    std::cout << score(x, y);    
+    };
+
+    Alien alien{2, 0};
+    std::cout << alien.x_coordinate << ", " << alien.y_coordinate;
 
     return 0;
 }
 
-int score(double x, double y){
-    const int CIRCLE_SIZE_OUTER {10};
-    const int CIRCLE_SIZE_MIDDLE{5};
-    const int CIRCLE_SIZE_INNER {1};
-
-    const int POINTS_MISS   {0};
-    const int POINTS_OUTER  {1};
-    const int POINTS_MIDDLE {5};
-    const int POINTS_INNER  {10};
-
-    double pos_on_target = std::sqrt((x * x) + (y * y));
-    if(pos_on_target <= CIRCLE_SIZE_INNER){
-        return POINTS_INNER;
-    }
-    else if(pos_on_target <= CIRCLE_SIZE_MIDDLE){
-        return POINTS_MIDDLE;
-    }
-    else if(pos_on_target <= CIRCLE_SIZE_OUTER){
-        return POINTS_OUTER;
-    }
-    else{
-        return POINTS_MISS;
-    }
-}
 
