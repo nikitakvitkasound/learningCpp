66ca81e2687fcc911d07ded0eba362536f2b7f13
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2024-10-02 23:57:37 +0400

    learningCpp 118: classes

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,24 +1,40 @@
 #include <iostream>
-#include <cmath>
+#include <string_view>
 
-int main(){
-    class Alien {
-        //todo accept x and y
-        public:
-        Alien(int new_x_coordinate, int new_y_coordinate){
-            int x_coordinate = new_x_coordinate;
-            int y_coordinate = new_y_coordinate;
-        }
-        int x_coordinate { };
-        int y_coordinate { };
-        
+enum AnimalType
+{
+    cat,
+    dog,
+    chicken,
+};
 
-    };
+constexpr std::string_view animalName(AnimalType type)
+{
+    switch (type)
+    {
+    case cat: return "cat";
+    case dog: return "dog";
+    case chicken: return "chicken";
+    default:  return "";
+    }
+}
 
-    Alien alien{2, 0};
-    std::cout << alien.x_coordinate << ", " << alien.y_coordinate;
-
-    return 0;
+constexpr int numLegs(AnimalType type)
+{
+    switch (type)
+    {
+    case cat: return 4;
+    case dog: return 4;
+    case chicken: return 2;
+    default:  return 0;
+    }
 }
 
 
+int main()
+{
+    constexpr AnimalType animal{ cat };
+    std::cout << "A " << animalName(animal) << " has " << numLegs(animal) << " legs\n";
+
+    return 0;
+}
