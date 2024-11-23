f18f3bf8f89ef56b5dbc852cac2b54dd0e5f8d86
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2024-11-23 21:14:55 +0400

    learningCpp 123: codewars tinkering

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,74 +1,16 @@
 #include <iostream>
-    enum class AccountStatus{
-        troll,
-        guest,
-        user,
-        mod
-    };
-    enum class Action {
-        read,
-        remove,
-        write
-    };
+#include <vector>
+#include <algorithm>
 
-    bool display_post(AccountStatus poster, AccountStatus viewer){
-        return poster != AccountStatus::troll || viewer == AccountStatus::troll;
-    }
-
-    bool permission_check(Action what, AccountStatus who){
-        bool permission { false };
-        switch (what)
-        {
-        case Action::read:
-            if(who == AccountStatus::troll) { permission = true; }
-            if(who == AccountStatus::guest) { permission = true; }
-            if(who == AccountStatus::user)  { permission = true; }
-            if(who == AccountStatus::mod)   { permission = true; }
-            return permission;
-        case Action::remove:
-            if(who == AccountStatus::troll) { permission = false; }
-            if(who == AccountStatus::guest) { permission = false; }
-            if(who == AccountStatus::user)  { permission = false; }
-            if(who == AccountStatus::mod)   { permission = true; }
-            return permission;
-        case Action::write:
-            if(who == AccountStatus::troll) { permission = true; }
-            if(who == AccountStatus::guest) { permission = false; }
-            if(who == AccountStatus::user)  { permission = true; }
-            if(who == AccountStatus::mod)   { permission = true; }
-            return permission;
-        default:
-            return permission;;
-        }
-    }
-
-    bool valid_player_combination(AccountStatus player_01, AccountStatus player_02){
-        if(player_01 == AccountStatus::guest || player_02 == AccountStatus::guest){
-            return false;
-        }
-        if(player_01 == AccountStatus::troll || player_02 == AccountStatus:: troll){
-            return false;
-        }
-        return true;
-    }
-
-    bool has_priority(AccountStatus rank_status_01, AccountStatus rank_status_02){
-        return rank_status_01 > rank_status_02;
-    }
+bool getSum(std::vector<int> nums, int elem) {
+    std::vector<int>::iterator search_result = std::find(nums.begin(), nums.end(), elem);
+    return search_result != nums.end();
+}
 
 int main(){
-
-    AccountStatus poster = AccountStatus::mod;
-    AccountStatus foster = AccountStatus::troll;
-    // Action action = Action::read;
-
+    std::vector<int> numbers {1, 2, -5, 14, 26, 0, -69};
     std::cout << std::boolalpha;
-    // std::cout << valid_player_combination(AccountStatus::user, AccountStatus::guest) << '\n';
-    std::cout << has_priority(AccountStatus::user, AccountStatus::troll) << '\n';
-    if(poster == foster){
-        std::cout << "Hi!";
-    }
-
+    std::cout << getSum(numbers, -5);
 
     return 0;
 }
