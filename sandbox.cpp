00fa29fe325c983e34914293ffc5c601e3965368
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2024-11-23 21:40:53 +0400

    learningCpp 124: digging deep into std::find() and std::find_of();

--- sandbox.cpp
+++ sandbox.cpp
@@ -2,15 +2,27 @@
 #include <vector>
 #include <algorithm>
 
-bool getSum(std::vector<int> nums, int elem) {
-    std::vector<int>::iterator search_result = std::find(nums.begin(), nums.end(), elem);
-    return search_result != nums.end();
+bool findNumber(const std::vector<int>& nums, const int find_number){
+    return std::find(nums.begin(), nums.end(), find_number) != nums.end();
+}
+
+int isGreaterThan(const std::vector<int>& nums, const int than_number){
+    auto search_result = std::find_if(nums.begin(), nums.end(), [than_number](int i) {return i > than_number;});
+    if(search_result != nums.end()){
+        return *search_result;
+    }
+    return -1;
 }
 
 int main(){
-    std::vector<int> numbers {1, 2, -5, 14, 26, 0, -69};
+    std::vector<int> numbers {12, 34, 45, 56, 78, 89, 90};
+    int a = 56;
+    int b = 50;
+    
     std::cout << std::boolalpha;
-    std::cout << getSum(numbers, -5);
+    
+    std::cout << findNumber(numbers, a) << '\n';
+    std::cout << isGreaterThan(numbers, b) << '\n';
 
     return 0;
 }
