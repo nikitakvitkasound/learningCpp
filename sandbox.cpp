315e90125a0ee72b0c699d880889a00f1c95df66
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2024-12-03 21:12:50 +0400

    learningCpp 128: STL

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,30 +1,45 @@
+// Working through CppCon 2018: Jonathan Boccara “105 STL Algorithms in Less Than an Hour” https://www.youtube.com/watch?v=2olsGf6JIkU
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
-
-std::vector<int> applyToEach(const std::vector<int>& numbers, const auto& lambda_function){
-    std::vector<int> applied_numbers;
-    std::transform(numbers.begin(), numbers.end(), std::back_inserter(applied_numbers), lambda_function);
-    return applied_numbers;
-}
+#include <numeric>
+// #include <utility>
 
 int main(){
+    std::vector<int> nums { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+    // std::sort(nums.begin(), nums.end(), std::greater{});
+    // std::stable_partition(nums.begin(), nums.end(), [](int i){return i % 2 == 0;});
+    // for(int item : nums){
+    //     std::cout << item << ' ';
+    // }
+    // std::vector<int> nums_2 { 2, 4, 5, 6, 8, 9, 10, 12, 28 };
+    // std::vector<int> result { };
+    // std::set_symmetric_difference(nums.begin(), nums.end(), nums_2.begin(), nums_2.end(), std::back_inserter(result));
+    // for (int item : result){
+    //     std::cout << item << ' ';
+    // }
 
-    constexpr int factor = 3;
-    constexpr auto scale_by_factor = [factor](int i){ return i *= factor;};
-    
-    std::vector<int> nums { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
-    std::vector<int> new_nums = applyToEach(nums, scale_by_factor);
-    
-    for (int item : nums){
+    // std::iota(nums.begin(), nums.end(), 42);
+    // nums.erase(std::remove(nums.begin(), nums.end(), 47), nums.end());
+    // for(int item : nums){
+    //     std::cout << item << ' ';
+    // }
+    std::vector<int> even { };
+    std::vector<int> odd { };
+    std::partition_copy(nums.begin(), nums.end(), std::back_inserter(even), std::back_inserter(odd), [](int i){return i % 2 == 0;});
+    for (int item : even){
         std::cout << item << ' ';
     }
-    
+    std::cout << "|";
+    for (int item : odd){
+        std::cout << item << ' ';
+    }
+    // int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    // int true_arr[5] = {0};
+    // int false_arr[5] = {0};
+ 
+    // std::partition_copy(std::begin(arr), std::end(arr), std::begin(true_arr), std::begin(false_arr), [](int i) { return 4 < i; });
+
     std::cout << '\n';
-    
-    for (int item : new_nums){
-        std::cout << item << ' ';
-    }
-    return 0;
 }
