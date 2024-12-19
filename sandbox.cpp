27adf895530b6d5ca9b1ad55407ae9045786819b
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2024-12-19 21:52:21 +0400

    learningCpp 144: classes

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
 
-class Test
+// Provide the definition for IntPair and the print() member function here
+struct IntPair
 {
-public:
-    int date { };
-    int fate { };
-    int bait { };
+    int first{};
+    int second{};
+    void print(){
+        std::cout << "Pair (" << first << ", " << second << ")\n";
+    }
+    bool isEqual(const IntPair& pair){
+        return (first == pair.first) && (second == pair.second);
+    }
 };
 
-int main(){
-    Test test {1, 2, 3};
-    std::cout << test.date << test.fate << test.bait;
+
+int main()
+{
+	IntPair p1 {1, 2};
+	IntPair p2 {3, 4};
+
+	std::cout << "p1: ";
+	p1.print();
+
+	std::cout << "p2: ";
+	p2.print();
+
+	std::cout << "p1 and p1 " << (p1.isEqual(p1) ? "are equal\n" : "are not equal\n");
+	std::cout << "p1 and p2 " << (p1.isEqual(p2) ? "are equal\n" : "are not equal\n");
+
+	return 0;
 }
