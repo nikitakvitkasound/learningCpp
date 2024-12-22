ef8cc87a765cc7240706370f85ee5f1fd7b2f4a6
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2024-12-22 18:08:17 +0400

    learningCpp 150: working with advanced classes and dynamic memory (book)

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,55 +1,34 @@
 #include <iostream>
-#include <cmath>
+#include <string>
 
-class Foo
+// do class Critter:
+    // accept name
+    // getname (to print);
+// do class Farm (vector of Critters (names)):
+    // initialize with reserved()
+    // Add() to add new member of the vector
+    // Roll() print vectors
+class Critter
 {
 private:
-    std::string_view m_name { };
-    int m_wins { };
-    int m_loss { };
-    static inline int s_Counter { 0 };
+    std::string m_name;
 public:
-    Foo(std::string_view name, int wins, int lose)
+    Critter(const std::string& name = "I don't know my name:(")
         : m_name { name }
-        , m_wins { wins }
-        , m_loss { lose }
     {
-        ++s_Counter;
     }
-
-    static int GetTotalInstances(){
-        return s_Counter;
-    }
-
-    void PrintWinLossRatio() const{
-        double ratio { static_cast<double>(m_wins) / static_cast<double>(m_loss) };
-        ratio = std::round(ratio * 100) * 0.01;
-        std::cout << m_name << ": " << m_wins << " wins, " << m_loss << " loss(es) or " << ratio << " WLR.\n";
-    }
-
-    void SetWins(int win){
-        m_wins += win;
-    }
-
-    void SetLosses(int loss){
-        m_loss += loss;
+    std::string GetName() const{
+        return m_name;
     }
 };
 
 
 
 int main(){
-    Foo fighter_01 {"Jackson Sackson", 18, 7};
-    fighter_01.SetLosses(2);
-    fighter_01.SetWins(3);
-    fighter_01.PrintWinLossRatio();
-    
-    Foo fighter_02 {"Mike Pyke", 29, 5};
-    Foo fighter_03 {"Test", 12, 22};
-    Foo fighter_04 {"Test", 12, 22};
-    Foo fighter_05 {"Test", 12, 22};
-    // fighter_02.SetLosses(2);
-    fighter_02.PrintWinLossRatio();
-    std::cout << Foo::GetTotalInstances();
+    Critter critter_01;
+    Critter critter_02 {"Gadget"};
+    std::cout << critter_01.GetName() << '\n';
+    std::cout << critter_02.GetName() << '\n';
 
+    return 0;
 }
