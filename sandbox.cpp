50e41191c878d1cc50735e5af5980ee01884d775
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2024-12-23 19:20:18 +0400

    learningCpp 154: overloading operator += and -= (ChatGPT exercise)

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,32 +1,39 @@
 #include <iostream>
 
-class Rectangle
+class Counter
 {
-    friend std::istream& operator>>(std::istream& is, Rectangle& rectangle){
-        std::cout << "Define Width: ";
-        is >> rectangle.m_width;
-        std::cout << "Define Height: ";
-        is >> rectangle.m_height;
-        return is;
+    friend Counter& operator+=(Counter& that, int increment){
+        that.m_value += increment;
+        if(that.m_value < 0){ 
+            that.m_value = 0; 
+        }
+        return that;
+    }
+    friend Counter& operator-=(Counter& that, int decrement){
+        that.m_value -= decrement;
+        if(that.m_value < 0){ 
+            that.m_value = 0; 
+        }
+        return that;
     }
 private:
-    int m_width {};
-    int m_height {};
+    int m_value {};
 public:
-    Rectangle(int width = 1, int height = 1)
-        : m_width { width }
-        , m_height { height }
-        {
-        }
-    int Area() const{
-        return m_width * m_height;
+    Counter (int value = 0)
+        : m_value { value }
+    {
+    }
+    int ShowValue() const{
+        return m_value;
     }
 };
 
 int main(){
-    Rectangle a;
-    std::cin >> a;
-    std::cout << "The area is " << a.Area() << '\n';
+    Counter a {22};
+    a += 4;
+    a -= 22; 
+    a -= 6;
+    std::cout << a.ShowValue();
 
     return 0;
 }
