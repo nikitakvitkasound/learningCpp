9db475384d6ff467e1802f4a0458dd095c6b104a
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2024-12-24 22:40:27 +0400

    learningCpp 156: heap and memory allocation

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,39 +1,21 @@
 #include <iostream>
 
-class Counter
-{
-    friend Counter& operator+=(Counter& that, int increment){
-        that.m_value += increment;
-        if(that.m_value < 0){ 
-            that.m_value = 0; 
-        }
-        return that;
-    }
-    friend Counter& operator-=(Counter& that, int decrement){
-        that.m_value -= decrement;
-        if(that.m_value < 0){ 
-            that.m_value = 0; 
-        }
-        return that;
-    }
-private:
-    int m_value {};
-public:
-    Counter (int value = 0)
-        : m_value { value }
-    {
-    }
-    int ShowValue() const{
-        return m_value;
-    }
-};
-
 int main(){
-    Counter a {22};
-    a += 4;
-    a -= 22; 
-    a -= 6;
-    std::cout << a.ShowValue();
+    int* ptr_int_01 {new int};
+    *ptr_int_01 = 7;
+    int* ptr_int_02 { nullptr };
+    if(!ptr_int_02){
+        ptr_int_02 = new int { 22 };
+        std::cout << "memory is allocated!\n";
+    }
+    std::cout << *ptr_int_01 <<' ' << *ptr_int_02 << '\n';
+
+    delete ptr_int_01;
+    ptr_int_01 = nullptr;
+    
+    delete ptr_int_02;
+    ptr_int_02 = nullptr;
+
 
     return 0;
 }
