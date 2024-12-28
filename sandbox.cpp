9c27c171277154625adbaa0ca77d348ca9f2784d
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2024-12-28 23:39:27 +0400

    learningCpp 160: working through book

--- sandbox.cpp
+++ sandbox.cpp
@@ -6,30 +6,38 @@ private:
     int m_speed { };
 public:
     Bot(const std::string& name, const int& speed)
-        : m_name { name } //{ new std::string {name} }
+        : m_name { name }
         , m_speed { speed }
     {
     }
 
-    // Bot(const Bot& copy_of)
-    // {
-    //     delete m_name;
-    //     m_name = new std::string {*copy_of.m_name};
-    //     m_speed = copy_of.m_speed;
-    // }
+    Bot(const Bot& copy_of)
+        : m_name { copy_of.m_name + "_copy" }
+        , m_speed { copy_of.m_speed }
+    {
+    }
+    
+    Bot& operator= (const Bot& copy_of){
+        m_name = copy_of.m_name + "_copy";
+        m_speed = copy_of.m_speed;
+        return *this; 
+    }
 
-    // ~Bot()
-    // {
-    //     delete m_name;
-    // }
-    // void Print() const{
-    //     std::cout << m_name << m_speed
-    // }
+    std::string GetName() const
+    {
+        return m_name;
+    }
 };
 
 int main(){
 
     Bot test {"Jack-9", 2};
+    Bot test_02 { test };
+    std::cout << test_02.GetName() << '\n';
+    Bot test_03 { "Steve", 5};
+    test_02 = test_03;
+    std::cout << test_02.GetName();
+
 
     return 0;
 }
