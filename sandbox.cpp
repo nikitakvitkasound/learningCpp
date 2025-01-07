2313554184339708542c7d09688ac9c3a3919f31
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2025-01-07 15:14:39 +0400

    learningCpp 168: virtual keyword (book)

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
 
-int main(){
-    std::cout << "Hello, World!\n" << std::endl;
+class Base
+{
+public:
+    int m_test {};
 
+    Base(int test = 0)
+        : m_test { test }
+    {
+        std::cout << "Base is created. Value is " << m_test << '\n';
+    }
+
+    virtual ~Base() = default;
+
+    void virtual GetTest() const
+    {
+        std::cout << m_test << " is test number of Base class.\n";
+    }
+};
+
+class Derived : public Base
+{
+public:
+    Derived (int test = 0)
+        : Base { test }
+    {
+    }
+
+    void virtual GetTest() const
+    {
+        std::cout << m_test << " is test number of Derived class.\n";
+        std::cout << "And . . . ";
+        Base::GetTest();
+    }
+
+};
+
+
+int main(){
+    std::cout << "Hello, World!\n";
+    Derived test { 12 };
+
+    test.GetTest();
     return 0;
 }
