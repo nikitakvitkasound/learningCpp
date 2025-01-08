dfed7fbd2b261a034a02fd1a1905ed634c1916ea
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2025-01-08 22:43:08 +0400

    learningCpp 172: google cpp update ex1

--- google_exercises/g_ch01_ex01.cpp
+++ google_exercises/g_ch01_ex01.cpp
@@ -2,41 +2,17 @@
 #include <iomanip>
 
 int main(){
-    int kFieldWidth { 30 };
-    std::string message { "Hello, World!" };
+    int kFieldWidth { 17 };
+    std::string message { "Hello World!" };
     
     for(int i = 0; i < 6; ++i){
-        std::cout << std::setw(kFieldWidth);
-        for(int i = 0; i < 4; ++i){
-            std::cout << message << ' ';
+        for(int j = 0; j < 4; ++j){
+            std::cout << std::left << std::setw(kFieldWidth) << message;
         }
         std::cout << '\n';
     }
 
+    std::cout << '\n';
     system("pause");
     return 0;
 }
-
-
-// - - - - Google solution - - - - // (didn't understand task right)
-
-/*
-#include <iostream>
-// we need the following include for setw() in some c++ implementations
-#include <iomanip>
-using namespace std;
-
-int main() {
-  // the first for-loop will handle the rows
-  for (int i = 0; i < 6; i++) {
-    // the second for loop will handle the columns
-    for (int j = 0;  j < 4 ; j++)
-      // setw(int) sets the column width
-      cout << setw(17) << "Hello World!";
-    // this  next line is a part of the first for loop
-    // and causes the new line
-    cout << endl;
-  }
-  return 0;
-}
-*/
