91515c8f26b5cde81da54511bb75011a13591b25
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2025-01-09 02:12:41 +0400

    learningCpp 179: google cpp update ex04_02 user input

--- google_exercises/g_ch01_ex04_02.cpp
+++ google_exercises/g_ch01_ex04_02.cpp
@@ -1,40 +1,42 @@
 // Euclidian Algorithm - a = b * q + r (remainder)
 #include <iostream>
 
-// todo input of nums;
-
 void GCD(int& a, int& b){
     int remainder { a % b };
-    if (remainder == 0){
-        a = b;
-        return;
-    }
     a = b;
     b = remainder;
+    
+    if(remainder == 0){
+        return;
+    }
     GCD(a, b);
 }
 
 int main(){
-    int number_01 { 1701 };
-    int number_02 { 3768 };
-
-    if(number_01 < number_02){
-        int temp { number_01 };
-        number_01 = number_02;
-        number_02 = temp;
-    }
-
-    GCD(number_01, number_02);
-
-    std::cout << "GCD is " << number_01 << '\n';
-
+    std::cout << "Do you want to know Greatest common divisor of two numbers?\n";
     
+    int number_01 {};
+    int number_02 {};
+    do{
+        std::cout << "Input two integer numbers separated by space, i.e. 12 25: ";
+        if(!(std::cin >> number_01 >> number_02)){
+            std::cin.clear();
+            std::cin.ignore(100000, '\n');
+            std::cout << "[ERROR]: only integer numbers are allowed! Try again!\n";
+            continue;
+        }
+        else{
+            std::cout << ">> gdc(" << number_01 << ", " << number_02 << ") is ";
+            GCD(number_01, number_02);
+            std::cout << number_01 << '\n';          
+            break;
+        }
+    }while(true);
 
     std::cout << '\n';
     system("pause");
     return 0;
 }
-
 
 
 /*
