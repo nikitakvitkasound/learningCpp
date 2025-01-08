680a50a44963d147899766867a8350ca40bf62d6
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2025-01-09 00:36:06 +0400

    learningCpp 176: google cpp ex04, update ex01

--- google_exercises/g_ch01_ex01.cpp
+++ google_exercises/g_ch01_ex01.cpp
@@ -2,12 +2,12 @@
 #include <iomanip>
 
 int main(){
-    int kFieldWidth { 17 };
+    const int FIELD_WIDTH { 17 };
     std::string message { "Hello World!" };
     
     for(int i = 0; i < 6; ++i){
         for(int j = 0; j < 4; ++j){
-            std::cout << std::left << std::setw(kFieldWidth) << message;
+            std::cout << std::left << std::setw(FIELD_WIDTH) << message;
         }
         std::cout << '\n';
     }
--- google_exercises/g_ch01_ex04.cpp
+++ google_exercises/g_ch01_ex04.cpp
@@ -1,15 +1,41 @@
-/*
-Create a main function that processes one guess from the player, and provides hints.
-Add what we need to allow for multiple guesses until the player guesses the number.
-*/
-
 #include <iostream>
 #include <random>
 
 int main(){
+    const int MIN_NUMBER { 1 };
+    const int MAX_NUMBER { 100 };
+    const int QUIT_NUMBER { -1 };
+    
     std::random_device rd_seed;
-    std::uniform_int_distribution<int> rd_generator {1, 100};
-    // int secret_number = rd_generator(rd_seed);
+    std::uniform_int_distribution<int> rd_generator {MIN_NUMBER, MAX_NUMBER};
+    int secret_number = rd_generator(rd_seed);
+    
+    int user_input {};
+    do{
+        std::cout << "Guess a number from " << MIN_NUMBER << " to " << MAX_NUMBER << " or " << QUIT_NUMBER << " to quit: ";
+        if(!(std::cin >> user_input)){
+            std::cin.clear();
+            std::cin.ignore(100000, '\n');
+            std::cout << "[ERROR]: only numbers from " << MIN_NUMBER << " to " << MAX_NUMBER << " or " << QUIT_NUMBER << " to quit!\n";
+            continue;
+        }
+
+        if(user_input == -1){
+            std::cout << "Okay, bye-bye! See you later!\n";
+            break;
+        }
+
+        if(user_input < secret_number){
+            std::cout << ">> Your number is less than the secret number . . . \n";
+        }
+        else if(user_input > secret_number){
+            std::cout << ">> Your number is larger than the secret number . . . \n";
+        }
+        else{
+            std::cout << "Congrats! You guessed it right! It's " << user_input << ", see you later!\n";
+        }
+    }while(user_input != secret_number && user_input != QUIT_NUMBER);
+
 
     std::cout << '\n';
     system("pause");
