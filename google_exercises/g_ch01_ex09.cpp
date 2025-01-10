7c744ebe9120901ee013c2b4ee017f4c413e949c
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2025-01-10 20:19:42 +0400

    learningCpp 183:  google cpp ex09 fstream (working on)

--- google_exercises/g_ch01_ex09.cpp
+++ google_exercises/g_ch01_ex09.cpp
@@ -1,20 +1,59 @@
-// // file.cpp, Maggie Johnson
-// // Description: An illustration of file processing
-// #include <fstream>
-// #include <iostream>
-// using namespace std;
+// my solution of 'Google' version of the program: 
+// 1.create file; 
+// 2.write to file; 
+// 3.read from file; 
+// 4.repeat steps 2 and 3;
 
-// int main() {
-//   char first_name[30], last_name[30]; int age;
-//   char file_name[20];
-//   // Collect the data.
-//   cout << "Enter First Name: "; cin >> first_name;
-//   cout << "Enter Last Name: "; cin >> last_name;
-//   cout << "Enter Age: "; cin >> age;
-//   cout << endl << "Enter the name of the file: "; cin >> file_name;
+#include <iostream>
+#include <fstream>
+//#include <ostream>
 
-//   // Create an ofstream called People, open the stream for output.
-//   ofstream People(file_name, ios::out);
-//   // Write the output to the stream.
-//   People << first_name << endl << last_name << endl << age << endl; return 0;
-// } 
+int main(){
+    // char file_name[20] {};
+    // std::string file_line {};
+    
+    // char person_first_name[30] {};
+    // char person_last_name[30] {};
+    // int person_age {};
+    // // int person_id {};
+
+    // std::cout << "Enter the name of the file (don't forget .txt at the end): ";
+    // std::cin >> file_name;
+    // std::ofstream outf_People(file_name);
+    // if(!outf_People){
+    //     std::cerr << "[ERROR]: a file could not be open for writing!\n";
+    //     return 1;
+    // }
+    // outf_People.close();
+
+    // std::fstream People(file_name, std::ios::in | std::ios::out);
+    // for(int i = 1; i < 11; ++i){
+    //     std::cout << "Enter First Name: ";
+    //     std::cin >> person_first_name;
+    //     std::cout << "Enter Last Name: ";
+    //     std::cin >> person_last_name;
+    //     std::cout << "Enter Age: ";
+    //     std::cin >> person_age;
+
+    //     People << i << ": " << person_first_name << ' ' << person_last_name  << ". Age: " << person_age << '\n';
+    //     std::getline(People, file_line);
+    //     std::cout << file_line;
+    // }
+
+    // return 0;
+
+    std::string file_name { "test.txt" };
+    std::fstream file_stream;
+    file_stream.open(file_name, std::ios::out);
+    file_stream.close();
+    file_stream.open(file_name, std::ios::in | std::ios::out);
+
+    file_stream << "Hello World!";
+
+    std::string text {};
+    file_stream.seekp(0);
+    std::getline(file_stream, text);
+    std::cout << text;
+
+    return 0;
+}
