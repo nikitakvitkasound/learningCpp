4edff675cb8a7f3b3c6fa8432b4b94111f63ef36
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2025-01-16 23:20:12 +0400

    learningCpp 195: google cpp ch02 exercise 02

--- google_exercises/g_ch02_exp02.cpp
+++ google_exercises/g_ch02_exp02.cpp
@@ -1,33 +1,51 @@
-// whlie loop and exit value
-// print() - book review, print() - final coast
-// funcs: need to order, total cost
-// get all dm through cin
-
 #include <iostream>
+
+struct Shop
+{
+    const double PCT_SALES_REQD_NEW { 0.9 };
+    const double PCT_SALES_REQD_USED { 0.65 };
+    const double PCT_SALES_OPT_NEW { 0.4 };
+    const double PCT_SALES_OPT_USED { 0.2 };
+
+    const double PCT_OFFLIST_PAY_BY_SHOP { 0.8 };
+};
 
 struct Book
 {
     int m_book_code {};
-    float m_book_price {};
+    double m_book_price {};
     int m_book_on_hand {};
     int m_book_need_to {};
     int m_book_reqd {}; //  1 - required, 0 - optional
     int m_book_new {}; //  1 - new, 0 - used
+    
+    int m_book_to_order {};
+    double m_total_cost {};
+    double m_profit {};
 };
 
-void SetBookInfo(Book& book);
-void GetBookInfo(const Book& book);
+void SetBookInfo(Book& bk);
+void GetBookInfo(const Book& bk);
+int GetApproxSellNumbers(const Book& bk, const Shop& shp);
+void GetOrderInfo(Book& bk, const Shop& shp);
+void GetProfitInfo(Book& bk, const Shop& shp);
+
 
 int main(){
+    Shop shop;
     Book book;
+
     int keep_going { 1 };
     while(keep_going){
         SetBookInfo(book);
+
         GetBookInfo(book);
-        
+        GetOrderInfo(book, shop);
+
         std::cout << "\nEnter '1' to do another book, '0' to stop: ";
         std::cin >> keep_going;
     }
+    GetProfitInfo(book, shop);
 
     std::cout << '\n';
     system("pause");
@@ -37,45 +55,79 @@ int main(){
 
 // - - - - Definition - - - - //
 
-void SetBookInfo(Book& book){
+void SetBookInfo(Book& bk){
     std::cout << "> Enter the book code: ";
-    std::cin >> book.m_book_code;
+    std::cin >> bk.m_book_code;
 
     std::cout << "> Enter single copy price: ";
-    std::cin >> book.m_book_price;
+    std::cin >> bk.m_book_price;
 
     std::cout << "> Enter number on hand of this book: ";
-    std::cin >> book.m_book_on_hand;
+    std::cin >> bk.m_book_on_hand;
 
     std::cout << "> Enter prospective enrollment to class: ";
-    std::cin >> book.m_book_need_to;
+    std::cin >> bk.m_book_need_to;
 
     std::cout << "> Enter '1' for 'required' or '0' for 'optional' book status: ";
-    std::cin >> book.m_book_reqd;
+    std::cin >> bk.m_book_reqd;
     
     std::cout << "> Enter '1' for 'new' or '0' for 'used' book status: ";
-    std::cin >> book.m_book_new;
+    std::cin >> bk.m_book_new;
 }
 
-void GetBookInfo(const Book& book)
+void GetBookInfo(const Book& bk)
 {
     std::cout << "\n*****************************************************\n";
-    std::cout << "\tBook code: " << book.m_book_code << '\n';
-    std::cout << "\tBook price: $" << book.m_book_price << '\n';
-    std::cout << "\tInventory: " << book.m_book_on_hand << " (on students' hands)\n";
-    std::cout << "\tClass enrollment: " << book.m_book_need_to << '\n';
+    std::cout << "\tBook code: " << bk.m_book_code << '\n';
+    std::cout << "\tBook price: $" << bk.m_book_price << '\n';
+    std::cout << "\tInventory: " << bk.m_book_on_hand << " (on students' hands)\n";
+    std::cout << "\tClass enrollment: " << bk.m_book_need_to << '\n';
     std::cout << "\tThis book is ";
-    if(book.m_book_reqd){
+    if(bk.m_book_reqd){
         std::cout << "'required' and ";
     }
     else{
         std::cout << "'optional' and ";
     }
-    if(book.m_book_new){
+    if(bk.m_book_new){
         std::cout << "'new'.";
     }
     else{
         std::cout << "'used'.";
     }
     std::cout << "\n*****************************************************\n";
+}
+
+int GetApproxSellNumbers(const Book& bk, const Shop& shp){
+    if(bk.m_book_reqd){
+        if(bk.m_book_new){
+            return static_cast<int>(bk.m_book_need_to * shp.PCT_SALES_REQD_NEW);
+        }
+        return static_cast<int>(bk.m_book_need_to * shp.PCT_SALES_REQD_USED);
+    }
+    else{
+        if(bk.m_book_new){
+            return static_cast<int>(bk.m_book_need_to * shp.PCT_SALES_OPT_NEW);
+        }
+        return static_cast<int>(bk.m_book_need_to * shp.PCT_SALES_OPT_USED);
+    }
+}
+
+void GetOrderInfo(Book& bk, const Shop& shp){
+    bk.m_book_to_order = GetApproxSellNumbers(bk, shp) - bk.m_book_on_hand;
+    bk.m_total_cost = bk.m_book_to_order * bk.m_book_price;
+
+    std::cout << "\n*****************************************************\n";
+    std::cout << "\tNeed to order: " << bk.m_book_to_order << " book(s).\n";
+    std::cout << "\tTotal order cost: $" << bk.m_total_cost << '\n';
+    std::cout << "*****************************************************\n";
+}
+
+void GetProfitInfo(Book& bk, const Shop& shp){
+    bk.m_profit = bk.m_total_cost - (bk.m_total_cost * shp.PCT_OFFLIST_PAY_BY_SHOP);
+
+    std::cout << "\n*****************************************************\n";
+    std::cout << "\tTotal for all orders: $" << bk.m_total_cost << '\n';
+    std::cout << "\tProfit: $" << bk.m_profit << '\n';
+    std::cout << "*****************************************************\n";
 }
