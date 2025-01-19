68f83055067a7e548fe20a19e350d0d5f8369a3c
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2025-01-19 20:00:51 +0400

    learningCpp 197: google cpp ch02 exercise 03

--- google_exercises/g_ch02_exp03.cpp
+++ google_exercises/g_ch02_exp03.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
+#include "g_composer.h"
+#include "g_user_interface.h"
+
+
 
 int main(){
+    std::string name {};
+    std::string name_last {};
+    std::string genre {};
+    std::string fact {};
+    int rank {};
+    int birth_year {};
+
+    UIcmd::GetInput("Enter composer' first name: ", name);
+    UIcmd::GetInput("Enter composer' last name: ", name_last);
+    UIcmd::GetInput("Enter composer' birth year: ", birth_year);
+    UIcmd::GetInput("Enter composer' genre: ", genre);
+    UIcmd::GetInput("Enter interesting fact about composer: ", fact);
+    UIcmd::GetInput("Enter composer' rank: ", rank);
+
+    Composer New {name, name_last, genre, fact, birth_year, rank};
+    
+    std::cout << "\n ************************************************** \n";
+    
+    New.Display();
+    
+
+    std::cout << '\n';
+    system("pause");
     return 0;
 }
