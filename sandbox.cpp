53bf6314daafa07a6d4307c379a935a8a9f50831
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2025-01-29 22:25:11 +0400

    learningCpp 210: COMP4200 live coding

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
+#include <fstream>
 
 class Point
 {
-    int m_x {};
-    int m_y {};
+    const int m_x {};
+    const int m_y {};
 
 public:
     Point();
     Point(int x, int y);
     int getX() const;
     int getY() const;
+    void print() const;
 };
 
 Point::Point()
-    : m_x { 0 }
-    , m_y { 0 }
 {
 }
 
@@ -34,12 +34,35 @@ int Point::getY() const
     return m_y;
 }
 
+void Point::print() const
+{
+    std::cout << "test 02: (" << m_x << ", " << m_y << ").\n"; 
+}
+
 int main(){
     Point test_01;
     Point test_02 {1, 2};
+    const Point test_03 {4, 12};
+
 
     std::cout << "test 01: (" << test_01.getX() << ", " << test_01.getY() << ").\n";
-    std::cout << "test 02: (" << test_02.getX() << ", " << test_02.getY() << ").\n"; 
+    std::cout << "test 02: (" << test_02.getX() << ", " << test_02.getY() << ").\n";
+    test_03.print();
+
+    // WORKING WITH FILES
+    // std::string filename{};
+    // std::ifstream fin(filename);
+    // std::string first;
+    // std::string last;
+    // int id;
+    // float avg;
+
+    // while(fin >> first){
+    //     fin >> last >> id >> avg;
+
+    //     // do something with there variables, i.e.
+    //     // addStudent(Student(first, last,id, avg));
+    // }
 
     return 0;
 }
