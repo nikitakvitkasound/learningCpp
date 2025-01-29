d0709984c9336a54f38343220996334e484ff92a
Nikita Kvitka <nikita.kvitka.sound@gmail.com>
2025-01-29 20:44:54 +0400

    learningCpp 209: COMP4200 live coding

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,11 +1,45 @@
 #include <iostream>
-#include <vector>
+
+class Point
+{
+    int m_x {};
+    int m_y {};
+
+public:
+    Point();
+    Point(int x, int y);
+    int getX() const;
+    int getY() const;
+};
+
+Point::Point()
+    : m_x { 0 }
+    , m_y { 0 }
+{
+}
+
+Point::Point(int x, int y)
+    : m_x { x }
+    , m_y { y }
+{
+}
+
+int Point::getX() const
+{
+    return m_x;
+}
+
+int Point::getY() const
+{
+    return m_y;
+}
 
 int main(){
-    int x {};
-    const int* ptr { &x };
+    Point test_01;
+    Point test_02 {1, 2};
 
-    std::cout << ptr << ' ' << (ptr + 1) << ' ' << (ptr + 2) << '\n';
+    std::cout << "test 01: (" << test_01.getX() << ", " << test_01.getY() << ").\n";
+    std::cout << "test 02: (" << test_02.getX() << ", " << test_02.getY() << ").\n"; 
 
     return 0;
 }
